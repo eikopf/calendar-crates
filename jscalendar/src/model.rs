4b58b6d39b556e0fadfa0ecfb77105804549530f
Oliver Wooding <oliverwooding@icloud.com>
2026-02-13 10:25:14 +0100

    jscalendar: added model::rrule to rexport rfc5545_types::rrule

--- jscalendar/src/model.rs
+++ jscalendar/src/model.rs
@@ -4,6 +4,11 @@ pub mod object;
 pub mod set;
 pub mod string;
 
+/// Recurrence rule types.
+pub mod rrule {
+    pub use rfc5545_types::rrule::*;
+}
+
 /// Date and time types.
 pub mod time {
     pub use calendar_types::{duration::*, primitive::*, time::*};
