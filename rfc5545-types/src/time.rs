feed77cdc1f09b3f62d2cdf9cab89b5b2de6a54b
Oliver Wooding <oliverwooding@icloud.com>
2026-02-17 14:32:01 +0100

    rfc5545-types: added UtcOffset

--- rfc5545-types/src/time.rs
+++ rfc5545-types/src/time.rs
@@ -1,9 +1,21 @@
 //! Basic time types.
 
-use calendar_types::time::{Date, DateTime};
+use calendar_types::{
+    primitive::Sign,
+    time::{Date, DateTime, Hour, Minute, NonLeapSecond},
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DateTimeOrDate<M> {
     DateTime(DateTime<M>),
     Date(Date),
+}
+
+/// An offset from UTC to some local time (RFC 5545 §3.3.14).
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct UtcOffset {
+    pub sign: Sign,
+    pub hour: Hour,
+    pub minute: Minute,
+    pub second: NonLeapSecond,
 }
