feeb7f8d777a22743ea8bd8b2bc34625855680ac
Oliver Wooding <oliverwooding@icloud.com>
2026-02-18 18:09:23 +0100

    jscalendar: updated Trigger to use <V as JsonValue>::Object for the Unknown variant

--- jscalendar/src/model/object.rs
+++ jscalendar/src/model/object.rs
@@ -8,7 +8,7 @@ use std::{
 use structible::structible;
 
 use crate::{
-    json::UnsignedInt,
+    json::{JsonValue, UnsignedInt},
     model::{
         request_status::{RequestStatus, StatusCode},
         rrule::RRule,
@@ -31,7 +31,7 @@ use crate::{
 /// A group is a collection of [`Event`] and [`Task`] objects. Typically, objects are grouped by
 /// topic (e.g. by keywords) or calendar membership.
 #[structible]
-pub struct Group<V> {
+pub struct Group<V: JsonValue> {
     // Group Properties (RFC 8984 §5.3)
     pub entries: Vec<TaskOrEvent<V>>,
     pub source: Option<Box<Uri>>,
@@ -57,10 +57,49 @@ pub struct Group<V> {
 }
 
 /// A [`Task`] or an [`Event`].
-#[derive(Debug, Clone, PartialEq)]
-pub enum TaskOrEvent<V> {
+pub enum TaskOrEvent<V: JsonValue> {
     Task(Task<V>),
     Event(Event<V>),
+}
+
+impl<V> PartialEq for TaskOrEvent<V>
+where
+    V: JsonValue + PartialEq,
+    V::Object: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Task(l0), Self::Task(r0)) => l0 == r0,
+            (Self::Event(l0), Self::Event(r0)) => l0 == r0,
+            _ => false,
+        }
+    }
+}
+
+impl<V> Clone for TaskOrEvent<V>
+where
+    V: JsonValue + Clone,
+    V::Object: Clone,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Self::Task(arg0) => Self::Task(arg0.clone()),
+            Self::Event(arg0) => Self::Event(arg0.clone()),
+        }
+    }
+}
+
+impl<V> std::fmt::Debug for TaskOrEvent<V>
+where
+    V: JsonValue + std::fmt::Debug,
+    V::Object: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Task(arg0) => f.debug_tuple("Task").field(arg0).finish(),
+            Self::Event(arg0) => f.debug_tuple("Event").field(arg0).finish(),
+        }
+    }
 }
 
 /// A JSCalendar event object (RFC 8984 §2.1).
@@ -69,7 +108,7 @@ pub enum TaskOrEvent<V> {
 /// reminder, or anniversary. It is required to start at a certain point in time and typically has
 /// a non-zero duration. Multiple participants may partake in the event at multiple locations.
 #[structible]
-pub struct Event<V> {
+pub struct Event<V: JsonValue> {
     // Event Properties (RFC 8984 §5.1)
     pub start: DateTime<Local>,
     pub duration: Option<Duration>,
@@ -136,7 +175,7 @@ pub struct Event<V> {
 /// at certain points in time, take some estimated time to complete, and recur, none of which is
 /// required.
 #[structible]
-pub struct Task<V> {
+pub struct Task<V: JsonValue> {
     // Task Properties (RFC 8984 §5.2)
     pub due: Option<DateTime<Local>>,
     pub start: Option<DateTime<Local>>,
@@ -360,7 +399,7 @@ pub struct SendToParticipant {
 
 /// A representation of an alert or a reminder (RFC 8984 §4.5.2).
 #[structible]
-pub struct Alert<V> {
+pub struct Alert<V: JsonValue> {
     pub trigger: Trigger<V>,
     pub acknowledged: Option<DateTime<Utc>>,
     pub related_to: Option<HashMap<Box<str>, Relation<V>>>,
@@ -371,11 +410,39 @@ pub struct Alert<V> {
 }
 
 /// The trigger of an [`Alert`].
-#[derive(Debug, Clone, PartialEq)]
-pub enum Trigger<V> {
+#[derive(PartialEq)]
+pub enum Trigger<V: JsonValue> {
     Offset(OffsetTrigger<V>),
     Absolute(AbsoluteTrigger<V>),
-    Unknown(V),
+    Unknown(V::Object),
+}
+
+impl<V> Clone for Trigger<V>
+where
+    V: JsonValue + Clone,
+    V::Object: Clone,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Self::Offset(arg0) => Self::Offset(arg0.clone()),
+            Self::Absolute(arg0) => Self::Absolute(arg0.clone()),
+            Self::Unknown(arg0) => Self::Unknown(arg0.clone()),
+        }
+    }
+}
+
+impl<V> std::fmt::Debug for Trigger<V>
+where
+    V: JsonValue + std::fmt::Debug,
+    V::Object: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Offset(arg0) => f.debug_tuple("Offset").field(arg0).finish(),
+            Self::Absolute(arg0) => f.debug_tuple("Absolute").field(arg0).finish(),
+            Self::Unknown(arg0) => f.debug_tuple("Unknown").field(arg0).finish(),
+        }
+    }
 }
 
 /// A trigger defined relative to a time property (RFC 8984 §4.5.2).
